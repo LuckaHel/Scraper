f9bb4785bf04e3e682fc521b7e850b3689ebc836
hel <leilakorsey@gmail.com>
2025-11-08 14:02:18 +0700

    feat:now creates json with party tutle and personal info of all memebers of parliament

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 import scrapy
 
+import re
+
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
@@ -10,6 +12,7 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         poslanci_list =  response.xpath('//div[@class="mps_list_block"]//li/a')
         member_dic = {}
+    
         for p in poslanci_list:
             # print(p)
             name = p.css('a::text').get()
@@ -17,10 +20,30 @@ class QuotesSpider(scrapy.Spider):
             profile_link = p.xpath('@href').get()
             if profile_link is not None:
                 yield response.follow(profile_link, callback=self.parse_profile)
+               
+            
         # print(member_dic)
+
     
     def parse_profile(self, response):
         membership = response.xpath('//div[@class="box"]//li/text()').get()
-        print(membership.split("(")[0])
+        personal_info = [
+            response.xpath('//div[@class="grid_4 omega"]//span').get(),
+            response.xpath('//div[@class="grid_4 alpha"]//span').get(),
+            response.xpath('//div[@class="grid_8 alpha omega"]//span').get()]
+        for i in range(len(personal_info)):
+            if personal_info[i] is None:
+                personal_info[i] = ""
+            else:
+                personal_info[i] = re.sub(r'<[^>]*>', '', personal_info[i]).strip()
+
+        yield {
+        "party": membership.split("(")[0].strip(),
+        "name_and_title": str(personal_info)
+    }
+        
+        
+
+        
 
             
