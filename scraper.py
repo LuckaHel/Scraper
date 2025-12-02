b7f8c980ea9b1d72a3bd52a2df952735dcea5d5e
hel <leilakorsey@gmail.com>
2025-12-02 13:35:25 +0700

    scraper now creates json, that has name titles party andlink to rozpravy for ech memebr of parliament

--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 import scrapy
-
+import json
 import re
 
 
@@ -11,19 +11,23 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         poslanci_list =  response.xpath('//div[@class="mps_list_block"]//li/a')
-        member_dic = {}
     
         for p in poslanci_list:
-            # print(p)
-            name = p.css('a::text').get()
-            # print(name)
             profile_link = p.xpath('@href').get()
             if profile_link is not None:
                 yield response.follow(profile_link, callback=self.parse_profile)
+        
+        self.count_rozpravy() 
                
             
-        # print(member_dic)
-
+    def count_rozpravy(self):
+        with open('raw.jsonl') as f:
+           for line in f:
+                record = json.loads(line)
+                print("type>>>>>>>>" + str(type(record)))
+                print("one ine when opening json >>>>>>>>>>>>" )
+                print(record)
+        
     
     def parse_profile(self, response):
         membership = response.xpath('//div[@class="box"]//li/text()').get()
@@ -31,19 +35,28 @@ class QuotesSpider(scrapy.Spider):
             response.xpath('//div[@class="grid_4 omega"]//span').get(),
             response.xpath('//div[@class="grid_4 alpha"]//span').get(),
             response.xpath('//div[@class="grid_8 alpha omega"]//span').get()]
+        
+        partial_link_to_rozpravy = response.xpath('//div[@class="box"]/ul[@class="aktivity"]/li').getall()
+        link_to_rozpravy = "https://www.nrsr.sk/web/" + re.search(r'href="([^"]*)"', partial_link_to_rozpravy[0]).group(1)
+        
         for i in range(len(personal_info)):
-            if personal_info[i] is None:
+            if personal_info[i] is None: #if tperson has no title replace none with empty string
                 personal_info[i] = ""
             else:
                 personal_info[i] = re.sub(r'<[^>]*>', '', personal_info[i]).strip()
+                
 
         yield {
         "party": membership.split("(")[0].strip(),
-        "name_and_title": str(personal_info)
+        "name_and_title": str(personal_info),
+        "link_to_rozpravy" :link_to_rozpravy   
     }
+    
+        
+    
         
         
 
-        
+    
 
             
